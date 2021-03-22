0101d028eb17a58f4643e196b92e9b8046d23511
AlexMatyas <Nianderil@yandex.ru>
2021-03-22 15:46:13 +0300

    Homework 1

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const goods = [
-    {title: 'Shirt', price: 150 },
-    {title: 'Socks', price: 50 },
-    {title: 'Jacket', price: 350 },
-    {title: 'Shoes', price: 250 },
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
 ]
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = someItem, price = 000) => { // можно убрать скобки и return
   return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 }
 
-const renderGoodsList = (list) => {
-        let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+const renderGoodsList = (list = []) => { // можно убрать круглые скобки, так как аргумент только один
+  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
   document.querySelector('.goods-list').innerHTML = goodsList;
 }
 
